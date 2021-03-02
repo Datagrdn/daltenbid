8d3bf3268c6f42036c7bbc6444c61f13a5925c1d
Jon Shapiro <jon@datagarden.org>
2021-03-02 12:18:09 -0800

    added babelrc for es6 imports

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,30 +1,8 @@
 import React, { Component } from "react";
+import { nftData } from "./nftData";
 
 class Gallery extends Component {
   render() {
-    let nftData = [
-      {
-        id: 1,
-        title: "Cool Dude 1",
-        image:
-          "https://cdn.shopify.com/s/files/1/2564/9670/products/Ballmoji_Cool_Dude_GD44-EC1_240x.png?v=1546226227",
-        chain: "eth",
-        artist: "tocrex-holpen",
-        topBid: "1.2",
-        topBidder: "nickname",
-      },
-      {
-        id: 2,
-        title: "Cool Dude 2",
-        image:
-          "https://cdn.shopify.com/s/files/1/2564/9670/products/Ballmoji_Cool_Dude_GD44-EC1_240x.png?v=1546226227",
-        chain: "eth",
-        artist: "tocrex-holpen",
-        topBid: "1.3",
-        topBidder: "nickname",
-      },
-    ];
-
     return (
       <div className="Main">
         <div className="container-fluid mt-5">
---
+++ src/components/nftData.js
@@ -0,0 +1,22 @@
+export const nftData = [
+  {
+    id: 1,
+    title: "Cool Dude 1",
+    image:
+      "https://cdn.shopify.com/s/files/1/2564/9670/products/Ballmoji_Cool_Dude_GD44-EC1_240x.png?v=1546226227",
+    chain: "eth",
+    artist: "tocrex-holpen",
+    topBid: "1.2",
+    topBidder: "nickname",
+  },
+  {
+    id: 2,
+    title: "Cool Dude 2",
+    image:
+      "https://cdn.shopify.com/s/files/1/2564/9670/products/Ballmoji_Cool_Dude_GD44-EC1_240x.png?v=1546226227",
+    chain: "eth",
+    artist: "tocrex-holpen",
+    topBid: "1.3",
+    topBidder: "nickname",
+  },
+];
