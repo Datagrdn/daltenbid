613ee93abbe6fe6ab1ae6a9afae4938479d41408
Jon Shapiro <jon@datagarden.org>
2021-03-02 23:10:21 -0800

    Bid popup only renders is localStorage email contains @ - about to refactor - still requires refresh

--- src/components/Bid.js
+++ src/components/Bid.js
@@ -6,40 +6,59 @@ export default class Bid extends Component {
   render() {
     return (
       <Popup trigger={<button>Place Bid</button>} position="top left">
-        {(close) => (
-          <div>
-            <div className="bid">
-              <table border="0" style={{ width: "100%" }}>
-                <tr>
-                  <th className="text-left" style={{ color: "#8B8B8B" }}>
-                    <small>Bid Amount: </small>
-                  </th>
-                  <th style={{ color: "#000000" }}>
-                    {" "}
-                    <input
-                      type="text"
-                      className="form-control form-control-sm text-white"
-                      placeholder="Bid Amount"
-                      onChange={(e) => console.log(e.target.value)}
-                      required
-                    />
-                  </th>
-                </tr>
-              </table>
-              <table style={{ width: "100%" }}>
-                <tr>
-                  <center>
-                    <button
-                      type="submit"
-                      className="btn btn-primary btn-sm btn-block"
+        {localStorage.getItem("emailInLocalStorage").includes("@") ? (
+          (close) => (
+            <div>
+              <div className="bid">
+                <table border="0" style={{ width: "100%" }}>
+                  <tr>
+                    <th>
+                      <center>
+                        <small>
+                          <b>{localStorage.getItem("emailInLocalStorage")}</b>
+                        </small>
+                      </center>
+                    </th>
+                  </tr>
+                </table>
+                <table border="0" style={{ width: "100%" }}>
+                  <tr>
+                    <th
+                      className="text-left"
+                      width="50%"
+                      style={{ color: "#8B8B8B" }}
                     >
-                      Place Bid
-                    </button>
-                  </center>
-                </tr>
-              </table>
+                      <small>Bid Amount: </small>
+                    </th>
+                    <th style={{ color: "#000000" }}>
+                      {" "}
+                      <input
+                        type="text"
+                        className="form-control form-control-sm text-white"
+                        placeholder="Bid Amount"
+                        onChange={(e) => console.log(e.target.value)}
+                        required
+                      />
+                    </th>
+                  </tr>
+                </table>
+                <table style={{ width: "100%" }}>
+                  <tr>
+                    <center>
+                      <button
+                        type="submit"
+                        className="btn btn-primary btn-sm btn-block"
+                      >
+                        Place Bid
+                      </button>
+                    </center>
+                  </tr>
+                </table>
+              </div>
             </div>
-          </div>
+          )
+        ) : (
+          <p>Please Enter E-mail</p>
         )}
       </Popup>
     );
