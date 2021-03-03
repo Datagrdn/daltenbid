0ee79754fda86d10d7bfbc23ced9be01109c427c
Jon Shapiro <jon@datagarden.org>
2021-03-02 20:44:20 -0800

    added basic input field that write email to localStorage

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,17 +2,29 @@ import React, { Component } from "react";
 import { nftData } from "./nftData";
 import { NftTable } from "./NftTable";
 
-class Gallery extends Component {
-  render() {
-    return (
-      <div className="Main">
-        <div className="container-fluid mt-5">
-          <img src="https://dalten.org/images/dalten-sigil.svg" />
-          <NftTable nftData={nftData} />
-        </div>
+const Gallery = () => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem("emailInLocalStorage") || ""
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem("emailInLocalStorage", value);
+  }, [value]);
+
+  const onChange = (event) => setValue(event.target.value);
+
+  return (
+    <div className="Main">
+      <div className="container-fluid mt-5">
+        <img src="https://dalten.org/images/dalten-sigil.svg" />
+        <br />
+        <br />
+        <input value={value} type="text" onChange={onChange} />
+        <p>{value}</p>
+        <NftTable nftData={nftData} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Gallery;
